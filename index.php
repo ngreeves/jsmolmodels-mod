3c7db2f49a1cdad5269bca79573ef22e57a81994
Carl LeBlond <carl.leblond@iup.edu>
2019-08-30 13:55:44 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+require_once "../config.php";
+// The Tsugi PHP API Documentation is available at:
+// http://do1.dr-chuck.com/tsugi/phpdoc/namespaces/Tsugi.html
+use \Tsugi\Core\Settings;
+use \Tsugi\Core\LTIX;
+// No parameter means we require CONTEXT, USER, and LINK
+$LAUNCH = LTIX::requireData(); 
+// Model
+$p = $CFG->dbprefix;
+// View
+$OUTPUT->header();
+$OUTPUT->bodyStart();
+$OUTPUT->flashMessages();
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <title>HTML5 Pacman</title>
+    
+    <style type="text/css">
+    </style>
+
+</head>
+
+<body>
+
+
+<?php include("jsmol.php"); ?>
+
+
+
+</body>
+</html>
+
+<?php
+$OUTPUT->footer();
+
---
+++ register.php
@@ -0,0 +1,7 @@
+<?php
+$REGISTER_LTI2 = array(
+"name" => "JSMol Models",
+"FontAwesome" => "fa-server",
+"short_name" => "JSMol Models",
+"description" => "3D model kit based on JSmol!"
+);
